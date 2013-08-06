3831834f55fe2ea41851349857385199056cd433
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-08-06 12:44:57 +0300

    Application initial skeleton added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bible::Application.config.secret_key_base = 'c21e9bc4e67334871ecddd5a3071edd26f4a4cf0e8cb88634f1e1a3bfa3676a4767ed8f4bc8edd1c603e1a0e1e6dda2accef7ab1c8d047ffd24e2ee552dfe730'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bible::Application.config.session_store :cookie_store, key: '_bible_session'
