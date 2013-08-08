c9cd6456582bad04fb775780aa51f00c319ab3c9
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-08-08 22:35:57 +0300

    Added mongoid configuration file

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
