d048167876313222440f71b4b44a51190d0bff67
Konstantin Rudy <konstantin.rudy@gmail.com>
2013-08-11 23:28:10 +0300

    Replaced configs with example configs

--- spec/models/bible/book_spec.rb
+++ spec/models/bible/book_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe Bible::Book do
   it { should have_many :verses }
+
+  it { should have_fields(:name, :title) }
+  it { should have_field(:chapters_count).of_type(Integer) }
+
 end
--- spec/models/bible/verse_spec.rb
+++ spec/models/bible/verse_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe Bible::Verse do
   it { should belong_to :book }
+
+  it { should have_fields :book_name, :text }
+  it { should have_field(:chapter).of_type(Integer) }
+
 end
